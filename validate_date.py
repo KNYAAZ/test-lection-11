245590bf5a7e4e6f071686b64b35628e6107f5bd
KNYAAZ <119792473+KNYAAZ@users.noreply.github.com>
2025-03-27 00:28:06 +0700

    Update validate_date.py

--- validate_date.py
+++ validate_date.py
@@ -1,10 +1,25 @@
 import re
 
-def is_valid_date(date_str):
-    pattern = r'^\d{1,2} (Янв|Фев|Мар|Апр|Май|Июн|Июл|Авг|Сен|Окт|Ноя|Дек), \d{4}$'
-    return bool(re.match(pattern, date_str))
+# Допустимые дни в месяце
+DAYS_IN_MONTH = {
+    "Янв": 31, "Фев": 29, "Мар": 31, "Апр": 30, "Май": 31, "Июн": 30,
+    "Июл": 31, "Авг": 31, "Сен": 30, "Окт": 31, "Ноя": 30, "Дек": 31
+}
 
-# Примеры
+def is_valid_date(date_str):
+    pattern = r'^(\d{1,2}) (Янв|Фев|Мар|Апр|Май|Июн|Июл|Авг|Сен|Окт|Ноя|Дек), (\d{4})$'
+    match = re.match(pattern, date_str)
+
+    if not match:
+        return False
+
+    day, month, year = int(match[1]), match[2], int(match[3])
+
+    # Проверяем, что день допустим для данного месяца
+    return 1 <= day <= DAYS_IN_MONTH[month]
+
+# Тесты
 print(is_valid_date("21 Янв, 1978"))  # True
 print(is_valid_date("32 Янв, 1978"))  # False
 print(is_valid_date("21 Jan, 1978"))  # False
+print(is_valid_date("30 Фев, 2000"))  # False (в феврале нет 30-го)
